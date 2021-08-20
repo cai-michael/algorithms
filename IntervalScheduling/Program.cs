7d1c9648386e706373c68e38b00299ffae45829c
cai-michael <mrcai@asu.edu>
2021-08-20 12:35:13 -0700

    Basic Project

---
+++ IntervalScheduling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntervalScheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
