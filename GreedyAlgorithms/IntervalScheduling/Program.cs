6e9b3683ae44dad443c62452b4551f817632f4a8
--global <--global>
2021-09-01 14:32:06 -0700

    Finished IntervalScheduling

--- GreedyAlgorithms/IntervalScheduling/Program.cs
+++ GreedyAlgorithms/IntervalScheduling/Program.cs
@@ -20,13 +20,25 @@ namespace IntervalScheduling
                 Job newJob = new Job(jobParams[0], int.Parse(jobParams[1]), int.Parse(jobParams[2]));
                 jobs.Add(newJob);
             }
+
+            // Add compatible jobs
+            List<Job> compatibleJobs = new List<Job>();
+            int mostRecentJobEnd = -1;
             jobs.Sort(new JobComparer());
             foreach (Job job in jobs)
             {
-                Console.WriteLine(job.end);
+                if (job.start > mostRecentJobEnd)
+                {
+                    compatibleJobs.Add(job);
+                    mostRecentJobEnd = job.end;
+                }
             }
-            
+
+            // Print largest subset of jobs
+            foreach (Job job in compatibleJobs)
+            {
+                Console.WriteLine(job.name);
+            }
         }
     }
-
 }
